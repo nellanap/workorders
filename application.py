709a2597cc1a8505cd5a7a8884f390248aa65906
Allen Pan <allenpan@Allens-MacBook-Air.local>
2019-02-02 00:24:12 -0500

    pushing to heroku

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 import os
 
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_moment import Moment
+from sqlalchemy import func
 from flask import Flask, flash, redirect, render_template, request, session
 from tempfile import mkdtemp
 from flask_session import Session
@@ -9,19 +12,12 @@ from flask_session import Session
 # Configure application
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+moment = Moment(app)
 
 
-class Update(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Text, nullable=True)
-    workorder_id = db.Column(db.Integer, db.ForeignKey('workorder.id'))
 
-    def __init__(self, status, workorder_id):
-        self.status = status
-        self.workorder_id = workorder_id
 
 class Workorder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,7 +25,7 @@ class Workorder(db.Model):
     type = db.Column(db.Text, nullable=True)
     description = db.Column(db.Text, nullable=True)
     closed = db.Column(db.Integer, nullable=True)
-    updates = db.relationship('Update', backref='workorder', lazy='dynamic')
+    updates = db.relationship('Update', backref='workorder', lazy='selectin')
 
     def __init__(self, apartment, type, description, closed):
         self.apartment = apartment
@@ -37,12 +33,21 @@ class Workorder(db.Model):
         self.description = description
         self.closed = closed
 
+class Update(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.Text, nullable=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    workorder_id = db.Column(db.Integer, db.ForeignKey('workorder.id'))
 
+
+    def __init__(self, status, workorder_id):
+        self.status = status
+        self.workorder_id = workorder_id
 
 
 # Creates a new database but "heroku pg:reset DATABASE" first
-# db.create_all()
-# db.session.commit()
+db.create_all()
+db.session.commit()
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -59,7 +64,18 @@ def after_request(response):
 @app.route("/")
 def index():
     """Show open workorders"""
-    open_workorders = Workorder.query.filter_by(closed=0)
+    #open_workorders = Workorder.query.filter_by(closed=0)
+    #open_workorders = (db.session.query(Workorder,Update).filter(Workorder.id == Update.workorder_id).filter_by(closed=0))
+    #open_workorders = db.session.query(Workorder, Update).join(Update)
+
+    #open_workorders = Workorder.query.join(Update, Workorder.id==Update.workorder_id).\
+    #    add_columns(Workorder.apartment, Workorder.type, Workorder.description, Update.status).\
+    #    filter(Workorder.closed==0)
+
+    open_workorders = db.session.query(Workorder).\
+        join(Update, Workorder.id==Update.workorder_id).\
+        add_columns(Workorder.apartment, Workorder.type, Workorder.description, Update.status, Update.timestamp).\
+        filter(Workorder.closed==0)
 
     return render_template("index.html", open_workorders=open_workorders)
 
@@ -77,6 +93,11 @@ def add():
         closed = 0
         new_workorder = Workorder(apartment, type, description, closed)
         db.session.add(new_workorder)
+        db.session.commit()
+
+        # Add new update
+        new_update = Update("Task Created", new_workorder.id)
+        db.session.add(new_update)
         db.session.commit()
 
         return redirect("/")
@@ -97,12 +118,27 @@ def close():
         closed_workorder.closed = 1
         db.session.commit()
 
-        return render_template("test.html", closed_workorder=closed_workorder)
+        # Add final update
+        status = request.form.get("status")
+        new_update = Update(status, workorder_id)
+        db.session.add(new_update)
+        db.session.commit()
+
+        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         open_workorders = Workorder.query.filter_by(closed=0)
         return render_template("close.html", open_workorders=open_workorders)
+
+@app.route("/history")
+def history():
+    """Show open workorders"""
+    #open_workorders = Workorder.query.filter_by(closed=0)
+    updates = Update.query.all()
+
+    return render_template("history.html", updates=updates)
+
 
 @app.route("/update", methods=["GET", "POST"])
 def update():
